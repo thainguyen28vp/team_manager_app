7a4712b0106bac2c817d68029740c9e587b18b3e
thainguyen28vp <thai2kvsvtvp@gmail.com>
2025-04-25 19:24:15 +0700

    icon app , splash

--- App.tsx
+++ App.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import AppContainer from './AppContainer'
+import SplashScreen from 'react-native-splash-screen'
+import { OS } from '@src/theme'
 
 const App = () => {
+  useEffect(() => {
+    if (OS === 'android') SplashScreen.hide()
+  }, [])
   return <AppContainer />
 }
 
--- android/app/src/main/java/com/team_manager_app/MainActivity.kt
+++ android/app/src/main/java/com/team_manager_app/MainActivity.kt
@@ -4,9 +4,13 @@ import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import org.devio.rn.splashscreen.SplashScreen;
+import android.os.Bundle;
 
 class MainActivity : ReactActivity() {
+
   override fun onCreate(savedInstanceState: Bundle?) {
+    SplashScreen.show(this);
     super.onCreate(null)
   }
   /**
